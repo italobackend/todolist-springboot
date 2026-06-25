4bf894b398f9fb47d84dba00b833e8fbd60dbf2a
Ítalo Freitas <italofreitas.work@gmail.com>
2026-06-25 15:33:52 -0300

    feat: inserção de métodos setters em User.java

--- src/main/java/com/italobackend/todolistspringboot/entity/User.java
+++ src/main/java/com/italobackend/todolistspringboot/entity/User.java
@@ -53,4 +53,16 @@ public class User {
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
