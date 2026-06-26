4fd64ad6b80f5c1638edaf29f727e3e92e33788c
Ítalo Freitas <italofreitas.work@gmail.com>
2026-06-26 13:57:13 -0300

    FEAT: Inserção métodos POST, DELETE, PATCH(nome) com relação as tasks.

--- src/main/java/com/italobackend/todolistspringboot/controller/TaskController.java
+++ src/main/java/com/italobackend/todolistspringboot/controller/TaskController.java
@@ -3,8 +3,6 @@ package com.italobackend.todolistspringboot.controller;
 import com.italobackend.todolistspringboot.dtos.TaskRequestDTO;
 import com.italobackend.todolistspringboot.entity.Task;
 import com.italobackend.todolistspringboot.service.TaskService;
-import com.italobackend.todolistspringboot.service.UserService;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,9 +25,25 @@ public class TaskController {
         return ResponseEntity.ok(taskService.listAllTasks());
     }
 
+    // POST - Criar tarefa
     @PostMapping
-    public ResponseEntity<Task> createTask(@RequestBody TaskRequestDTO taskRequestDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(taskRequestDTO));
+    public ResponseEntity<Task> createTask(@RequestBody TaskRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(dto));
+    }
+
+    // DELETE - Deletar tarefa
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    // PATCH - Atualizar nome
+    @PatchMapping("/{id}/title")
+    public ResponseEntity<Void> editTitle(@PathVariable Long id, @RequestBody TaskRequestDTO dto) {
+        taskService.editTitleTask(id, dto);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 
